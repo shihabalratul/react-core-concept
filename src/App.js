b0c01beb0e5f003dfda72549a74ab4b74cf9cca2
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-23 16:58:01 +0100

    Making dynamic using react

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products=[
+    {name:'Photoshop', price:'$90.99'},
+    {name:'Illustrator', price:'$60.20'},
+    {name:'InDesign', price:'$20.50'}
+  ]
+  const players=['Sakib','Mushfiq','Mashrafi','Mustafiz','Miraz', 'Tamim'];
   return (
     <div className="App">
       <header className="App-header">
       <h1>I am a react guy.</h1>
+      {
+      products.map(pd => <Product product={pd}></Product>)
+      }
+      <ol>
+        {players.map(name => <li>{name}</li>)}
+      </ol>
+      
       <Person name='Shihab Al Ratul' profession='Web-developer'></Person>
       <Person name='Abdun Nafi' profession='Graphics Designer'></Person>
       <Person name='Jhony Sheikh' profession='Dokandar'></Person>
@@ -14,9 +27,25 @@ function App() {
   );
 }
 
+function Product(props){
+  const {name, price}= props.product;
+  const style = {
+    margin:"2px",
+    padding:"10px",
+    backgroundColor:"gray"
+  }
+  console.log(name, price)
+  return(
+    <div style={style}>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <button>Buy Now</button>
+    </div>
+  )
+}
 function Person(props){
   return(
-    <div style={{border:'1px solid red', margin:'5px', backgroundColor:'green'}}>
+    <div style={{border:'2px solid red', margin:'5px'}}>
       <h3>Name: {props.name}</h3>
       <p>Profession: {props.profession}</p>
     </div>
