d8dcb5025a01e16673de290fe1b95a5d14099d22
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-23 15:21:55 +0100

    first time react use

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>I am a react guy.</h1>
+      <Person name='Shihab Al Ratul' profession='Web-developer'></Person>
+      <Person name='Abdun Nafi' profession='Graphics Designer'></Person>
+      <Person name='Jhony Sheikh' profession='Dokandar'></Person>
       </header>
     </div>
   );
 }
 
+function Person(props){
+  return(
+    <div style={{border:'1px solid red', margin:'5px', backgroundColor:'green'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Profession: {props.profession}</p>
+    </div>
+  )
+}
 export default App;
